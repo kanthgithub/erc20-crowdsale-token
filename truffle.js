69b2dc1727a027f78af9c3e9ad6e613968f209b9
Matt Shipman <shippy@littlephil.com.au>
2018-05-08 11:21:00 +1000

    Add config package and include use where needed

--- truffle.js
+++ truffle.js
@@ -1,19 +1,22 @@
-require('dotenv').config();
+const config = require('config');
+
+// Get config variables
+const ownerWallet = config.get('OWNER_WALLET');
 
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 7545,
-      gas: 6500000,
-      network_id: "5777"
-    },
-    rinkeby: {
-      host: "localhost", // Connect to geth on the specified
-      port: 8545,
-      from: process.env.OWNER_WALLET, // default address to use for any transaction Truffle makes during migrations
-      network_id: 4,
-      gas: 6712390 // Gas limit used for deploys
+    networks: {
+        development: {
+          host: "localhost",
+          port: 7545,
+          gas: 6500000,
+          network_id: "5777"
+        },
+        rinkeby: {
+          host: "localhost", // Connect to geth on the specified
+          port: 8545,
+          from: ownerWallet, // default address to use for any transaction Truffle makes during migrations
+          network_id: 4,
+          gas: 6712390 // Gas limit used for deploys
+        }
     }
-  }
 };
