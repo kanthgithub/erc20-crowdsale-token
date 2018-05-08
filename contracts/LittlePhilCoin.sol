8897f8fb9fca6fef180f25eda1684af961920238
mathewsayed <mathew.sayed@gmail.com>
2018-05-08 17:16:05 +1000

    override ownership for token crowdsale

--- contracts/LittlePhilCoin.sol
+++ contracts/LittlePhilCoin.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.21;
 
 import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 
-contract LittlePhilCoin is MintableToken {
+contract LittlePhilCoin is MintableToken, PausableToken {
     string public name = "Little Phil Coin";
     string public symbol = "LPC";
     uint8 public decimals = 18;
