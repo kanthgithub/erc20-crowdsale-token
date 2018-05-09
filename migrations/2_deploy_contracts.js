5cd62ca5c33ff77d1130d911cfc0734549560498
mathewsayed <mathew.sayed@gmail.com>
2018-05-09 16:31:52 +1000

    added capped crowdsale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,6 +6,7 @@ const LittlePhilCoin = artifacts.require("./LittlePhilCoin.sol");
 module.exports = function(deployer) {
     const rate = new web3.BigNumber(1000);
     const wallet = config.get("MULTISIG_WALLET"); // receiver multisig wallet for Eth
+    const cap = config.get("CROWDSALE_HARDCAP_WEI");
 
     return deployer
         .then(() => {
@@ -16,7 +17,8 @@ module.exports = function(deployer) {
             LittlePhilCrowdsale,
             rate,
             wallet,
-            LittlePhilCoin.address
+            LittlePhilCoin.address,
+            cap
         );
     });
 };
