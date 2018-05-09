ccbee4ab72f8229a91b46506ce30b10cfce4afca
mathewsayed <mathew.sayed@gmail.com>
2018-05-09 11:42:48 +1000

    added goal, hard cap for crowdsale inheritance

--- contracts/LittlePhilCrowdsale.sol
+++ contracts/LittlePhilCrowdsale.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.4.21;
 import "./LittlePhilCoin.sol";
 import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/validation/WhitelistedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 
-contract LittlePhilCrowdsale is MintedCrowdsale, WhitelistedCrowdsale { 
-    constructor(uint256 _rate, address _wallet, MintableToken _token) public
-    Crowdsale(_rate, _wallet, _token) {}
+contract LittlePhilCrowdsale is MintedCrowdsale, WhitelistedCrowdsale, CappedCrowdsale, RefundableCrowdsale { 
+    
+    // Constructor
+    constructor(uint256 _rate, address _wallet, MintableToken _token)
+    Crowdsale(_rate, _wallet, _token) CappedCrowdsale(_cap) RefundableCrowdsale(_goal) public {
+        // Goal 5000 ETH (5000000000000000000000) Cap 26527.206 ETH (26527206000000000000000)
+        require(_goal <= _cap);
+    }
 
+
+    // Ownership management
     function transferOwnership(address _newOwner) public onlyOwner {
         require(msg.sender == owner); // Only the owner of the crowdsale contract should be able to call this function.
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,24 @@
+const config = require("config");
+
 const LittlePhilCrowdsale = artifacts.require("./LittlePhilCrowdsale.sol");
 const LittlePhilCoin = artifacts.require("./LittlePhilCoin.sol");
 
 module.exports = function(deployer) {
-  const rate = new web3.BigNumber(1000);
-  const wallet = "0xe35FEf8d3E0BDBe34318aAf2611e40ACc29FaAB6"; // receiver multisig wallet for Eth
+    const rate = new web3.BigNumber(1000);
+    const wallet = config.get("MULTISIG_WALLET"); // receiver multisig wallet for Eth
+    const goal = config.get("MINIMUM_GOAL_WEI"); //amount in wei
+    const cap = config.get("CROWDSALE_HARDCAP_WEI"); //amount in wei
 
-  return deployer
-    .then(() => {
-      return deployer.deploy(LittlePhilCoin);
-    })
-    .then(() => {
-      return deployer.deploy(
-        LittlePhilCrowdsale,
-        rate,
-        wallet,
-        LittlePhilCoin.address
-      );
+    return deployer
+        .then(() => {
+            return deployer.deploy(LittlePhilCoin);
+        })
+        .then(() => {
+            return deployer.deploy(
+            LittlePhilCrowdsale,
+            rate,
+            wallet,
+            LittlePhilCoin.address
+        );
     });
 };
